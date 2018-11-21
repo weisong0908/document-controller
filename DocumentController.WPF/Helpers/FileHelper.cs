0759039a4c75b9ed446779fda29626db1587d79e
Teng Wei Song <hi@tengweisong.com>
2018-11-21 19:45:02 +0800

    Add the function to update files

--- DocumentController.WPF/Helpers/FileHelper.cs
+++ DocumentController.WPF/Helpers/FileHelper.cs
@@ -2,6 +2,8 @@
 using DocumentController.WPF.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,10 +40,110 @@ namespace DocumentController.WPF.Helpers
                     break;
             }
 
-            string folderPath = System.IO.Path.Combine(sharedDrive, mainFolder);
-            string fullPath = System.IO.Path.Combine(folderPath, $"{document.Title} - V{document.VersionNumber}");
+            string folderPath = Path.Combine(sharedDrive, mainFolder);
+            string fullPath = Path.Combine(folderPath, $"{document.Title} - V{document.VersionNumber}");
 
             return fullPath;
+        }
+
+        private static (string publicPDF, string publicEditable, string privateCurrentPDF, string privateCurrentEditable, string privateObselete) GetUploadPaths(DocumentViewModel document)
+        {
+            string publicPDF = "";
+            string publicEditable = "";
+            string privateCurrentPDF = "";
+            string privateCurrentEditable = "";
+            string privateObselete = "";
+
+            switch (document.Type)
+            {
+                case DocumentType.Policy:
+                    publicPDF = Path.Combine(@"# Curtin Singapore Corporate Policies #", document.Department);
+                    privateCurrentPDF = Path.Combine(document.Department, @"Current\Pol\PDF");
+                    privateCurrentEditable = Path.Combine(document.Department, @"Current\Pol\Editable");
+                    privateObselete = Path.Combine(document.Department, @"Obselete\Pol");
+                    break;
+                case DocumentType.Procedure:
+                    publicPDF = Path.Combine(@"# Curtin Singapore Corporate Procedures #", document.Department);
+                    privateCurrentPDF = Path.Combine(document.Department, @"Current\Pro\PDF");
+                    privateCurrentEditable = Path.Combine(document.Department, @"Current\Pro\Editable");
+                    privateObselete = Path.Combine(document.Department, @"Obselete\Pro");
+                    break;
+                case DocumentType.Form:
+                    publicPDF = Path.Combine(@"= Controlled Document =\Forms & Templates", document.Department);
+                    publicEditable = Path.Combine(@"= Controlled Document =\Forms & Templates", document.Department);
+                    privateCurrentPDF = Path.Combine(document.Department, @"Current\F & T\PDF");
+                    privateCurrentEditable = Path.Combine(document.Department, @"Current\F & T\Editable");
+                    privateObselete = Path.Combine(document.Department, @"Obselete\F & T");
+                    break;
+                case DocumentType.WorkInstruction:
+                    publicPDF = Path.Combine(@"= Controlled Document =\Work Instructions & Guidelines", document.Department);
+                    privateCurrentPDF = Path.Combine(document.Department, @"Current\WI & GL\PDF");
+                    privateCurrentEditable = Path.Combine(document.Department, @"Current\WI & GL\Editable");
+                    privateObselete = Path.Combine(document.Department, @"Obselete\WI & GL");
+                    break;
+                case DocumentType.OrganisationChart:
+                    publicPDF = Path.Combine(@"= Controlled Document =", "Organisation Chart");
+                    privateCurrentPDF = Path.Combine(document.Department, @"Current\Organisation Chart\PDF");
+                    privateCurrentEditable = Path.Combine(document.Department, @"Current\Organisation Chart\Editable");
+                    privateObselete = Path.Combine(document.Department, @"Obselete\Organisation Chart");
+                    break;
+            }
+
+            return (publicPDF, publicEditable, privateCurrentPDF, privateCurrentEditable, privateObselete);
+        }
+
+        private static void DeleteFile(DocumentViewModel document, string folder)
+        {
+            var filesToDelete = Directory.GetFiles(folder, document.Title + "*", SearchOption.TopDirectoryOnly);
+
+            if (filesToDelete.Length == 0)
+                return;
+
+            File.Delete(filesToDelete[0]);
+        }
+
+        private static void MoveFile(DocumentViewModel document, string sourceFolder, string destinationFolder)
+        {
+            var searchResults = Directory.GetFiles(sourceFolder, document.Title + "*", SearchOption.TopDirectoryOnly);
+
+            if (searchResults.Length == 0)
+                return;
+
+            var fileToMove = Path.GetFileName(searchResults[0]);
+
+            File.Move(Path.Combine(sourceFolder, fileToMove), Path.Combine(destinationFolder, fileToMove));
+
+            Process.Start(sourceFolder);
+            Process.Start(destinationFolder);
+        }
+
+        private static void CopyFile(DocumentViewModel document, DocumentVersionViewModel documentVersion, string sourceFileName, string destinationFolder)
+        {
+            var fileToCopy = document.Title + " - V" + documentVersion.VersionNumber + Path.GetExtension(sourceFileName);
+            var destinationFileName = Path.Combine(destinationFolder, fileToCopy);
+
+            if (File.Exists(sourceFileName))
+                File.Copy(sourceFileName, destinationFileName);
+
+            Process.Start(destinationFolder);
+        }
+
+        public static void UpdateFiles(DocumentViewModel document, DocumentVersionViewModel documentVersion)
+        {
+            var (publicPDF, publicEditable, privateCurrentPDF, privateCurrentEditable, privateObselete) = GetUploadPaths(document);
+
+            DeleteFile(document, publicPDF);
+            CopyFile(document, documentVersion, documentVersion.Location_PDF, publicPDF);
+            MoveFile(document, privateCurrentPDF, privateObselete);
+            CopyFile(document, documentVersion, documentVersion.Location_PDF, privateCurrentPDF);
+            MoveFile(document, privateCurrentEditable, privateObselete);
+            CopyFile(document, documentVersion, documentVersion.Location_Editable, privateCurrentEditable);
+
+            if (document.Type == DocumentType.Form)
+            {
+                DeleteFile(document, publicEditable);
+                CopyFile(document, documentVersion, documentVersion.Location_Editable, publicEditable);
+            }
         }
     }
 }
