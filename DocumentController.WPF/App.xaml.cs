3fdd62facd689cb81cdb57cc4b4dc8c32dcd8799
Teng Wei Song <hi@tengweisong.com>
2018-11-23 10:03:58 +0800

    Implement LocalDocumentVersionService

--- DocumentController.WPF/App.xaml.cs
+++ DocumentController.WPF/App.xaml.cs
@@ -17,10 +17,11 @@ namespace DocumentController.WPF
     public partial class App : Application
     {
         public string ConnectionString { get; } = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\\csing.navitas.local\shared\Documents\z-Public\QA and Compliance\Controlled Document\Controlled Document Master List.mdb;Persist Security Info=False;Jet OLEDB:Database Password=1234;";
-        //public string SharedDrive { get; set; } = @"\\csing.navitas.local\shared\Documents\";
-        public string SharedDrive { get;} = @"C:\Users\weisong.teng\Desktop\S\";
-        //public string ArchivedFolder { get; set; } = @"\\csing.navitas.local\shared\Documents\Quality Assurance\#QA & COMPLIANCE Dept Functions#\Controlled Document\";
-        public string ArchivedFolder { get; } = @"C:\Users\weisong.teng\Desktop\S\Quality Assurance\#QA & COMPLIANCE Dept Functions#\Controlled Document\";
+        //public string ConnectionString { get; } = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\weisong.teng\Desktop\Controlled Document Master List.mdb;Persist Security Info=False;Jet OLEDB:Database Password=1234;";
+        public string SharedDrive { get; set; } = @"\\csing.navitas.local\shared\Documents\";
+        //public string SharedDrive { get;} = @"C:\Users\weisong.teng\Desktop\S\";
+        public string ArchivedFolder { get; set; } = @"\\csing.navitas.local\shared\Documents\Quality Assurance\#QA & COMPLIANCE Dept Functions#\Controlled Document\";
+        //public string ArchivedFolder { get; } = @"C:\Users\weisong.teng\Desktop\S\Quality Assurance\#QA & COMPLIANCE Dept Functions#\Controlled Document\";
 
         public IDocumentService DocumentService;
         public IDocumentVersionService DocumentVersionService;
@@ -28,7 +29,7 @@ namespace DocumentController.WPF
         protected override void OnStartup(StartupEventArgs e)
         {
             DocumentService = new LocalDocumentService();
-            DocumentVersionService = new FakeDocumentVersionService();
+            DocumentVersionService = new LocalDocumentVersionService();
             Mapper.Initialize(c => c.AddProfile<MappingProfile>());
 
             var startupWindow = new Views.DocumentsWindow();
---
+++ DocumentController.WPF/Services/LocalDocumentVersionService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DocumentController.WPF.Models;
+
+namespace DocumentController.WPF.Services
+{
+    public class LocalDocumentVersionService : IDocumentVersionService
+    {
+        private OleDbConnection dbConnection;
+        private OleDbDataReader dataReader;
+        private OleDbCommand command;
+
+        public LocalDocumentVersionService()
+        {
+            dbConnection = new OleDbConnection((Application.Current as App).ConnectionString);
+        }
+
+        public async Task<IEnumerable<DocumentVersion>> GetAllVersionsByDocumentId(int documentId)
+        {
+            dbConnection.Open();
+            command = new OleDbCommand("SELECT * FROM Versions WHERE Doc_ID =" + documentId, dbConnection);
+            dataReader = command.ExecuteReader();
+
+            var documentVersions = new List<DocumentVersion>();
+
+            while (await dataReader.ReadAsync())
+            {
+                documentVersions.Add(new DocumentVersion()
+                {
+                    Id = int.Parse(dataReader["ID"].ToString()),
+                    DocumentId = int.Parse(dataReader["Doc_ID"].ToString()),
+                    VersionNumber = dataReader["Version"].ToString(),
+                    EffectiveDate = (dataReader["Effective_Date"] == DBNull.Value) ? DateTime.MinValue : DateTime.Parse(dataReader["Effective_Date"].ToString()),
+                    Progress = dataReader["Update_Status"].ToString(),
+                    DescriptionOfChange = dataReader["Description_of_Change"].ToString(),
+                    PurposeOfChange = dataReader["Purpose_of_Change"].ToString(),
+                    Requestor = dataReader["Requestor"].ToString(),
+                    Remarks = dataReader["Remarks"].ToString(),
+                    Location_PDF = dataReader["Location_PDF"].ToString(),
+                    Location_Editable = dataReader["Location_Editable"].ToString(),
+                    IsRemoved = dataReader["IsRemoved"].ToString().ToLower()
+                });
+            }
+
+            dataReader.Close();
+            dbConnection.Close();
+
+            return documentVersions;
+        }
+
+        public async Task<DocumentVersion> AddNewDocumentVersion(DocumentVersion documentVersion)
+        {
+            string sql = "INSERT INTO Versions " +
+                "(Doc_ID, Version, Effective_Date, Update_Status, Description_of_Change, Purpose_of_Change, " +
+                "Requestor, Remarks, Location_PDF, Location_Editable) VALUES " +
+                "(@DocumentId, @VersionNumber, @EffectiveDate, @Progress, @DescriptionOfChange, @PurposeOfChange, " +
+                "@Requestor, @Remarks, @Location_PDF, @Location_Editable)";
+
+            dbConnection.Open();
+            command = new OleDbCommand(sql, dbConnection);
+
+            OleDbParameter documentId = new OleDbParameter("@DocumentId", documentVersion.DocumentId);
+            OleDbParameter versionNumber = new OleDbParameter("@VersionNumber", documentVersion.VersionNumber);
+            OleDbParameter effectiveDate = new OleDbParameter("@EffectiveDate", documentVersion.EffectiveDate);
+            OleDbParameter progress = new OleDbParameter("@Progress", documentVersion.Progress);
+            OleDbParameter descriptionOfChange = new OleDbParameter("@DescriptionOfChange", documentVersion.DescriptionOfChange);
+            OleDbParameter purposeOfChange = new OleDbParameter("@PurposeOfChange", documentVersion.PurposeOfChange);
+            OleDbParameter requestor = new OleDbParameter("@Requestor", documentVersion.Requestor);
+            OleDbParameter remarks = new OleDbParameter("@Remarks", documentVersion.Remarks);
+            OleDbParameter location_PDF = new OleDbParameter("@Location_PDF", documentVersion.Location_PDF);
+            OleDbParameter location_Editable = new OleDbParameter("@Location_Editable", documentVersion.Location_Editable);
+
+            command.Parameters.Add(documentId);
+            command.Parameters.Add(versionNumber);
+            command.Parameters.Add(effectiveDate);
+            command.Parameters.Add(progress);
+            command.Parameters.Add(descriptionOfChange);
+            command.Parameters.Add(purposeOfChange);
+            command.Parameters.Add(requestor);
+            command.Parameters.Add(remarks);
+            command.Parameters.Add(location_PDF);
+            command.Parameters.Add(location_Editable);
+
+            await command.ExecuteNonQueryAsync();
+            dbConnection.Close();
+
+            return documentVersion;
+        }
+
+        public async Task<DocumentVersion> UpdateDocumentVersion(DocumentVersion documentVersion)
+        {
+            string sql = "UPDATE Versions SET " +
+                "Doc_ID = @DocumentId, " +
+                "Version = @VersionNumber, " +
+                "Effective_Date = @EffectiveDate, " +
+                "Update_Status = @Progress, " +
+                "Description_of_Change = @DescriptionOfChange, " +
+                "Purpose_of_Change = @PurposeOfChange, " +
+                "Requestor = @Requestor, " +
+                "Remarks = @Remarks, " +
+                "Location_PDF = @Location_PDF, " +
+                "Location_Editable = @Location_Editable " +
+                "WHERE ID = " + documentVersion.Id;
+
+            dbConnection.Open();
+            command = new OleDbCommand(sql, dbConnection);
+
+            OleDbParameter documentId = new OleDbParameter("@DocumentId", documentVersion.DocumentId);
+            OleDbParameter versionNumber = new OleDbParameter("@VersionNumber", documentVersion.VersionNumber);
+            OleDbParameter effectiveDate = new OleDbParameter("@EffectiveDate", documentVersion.EffectiveDate);
+            OleDbParameter progress = new OleDbParameter("@Progress", documentVersion.Progress);
+            OleDbParameter descriptionOfChange = new OleDbParameter("@DescriptionOfChange", documentVersion.DescriptionOfChange);
+            OleDbParameter purposeOfChange = new OleDbParameter("@PurposeOfChange", documentVersion.PurposeOfChange);
+            OleDbParameter requestor = new OleDbParameter("@Requestor", documentVersion.Requestor);
+            OleDbParameter remarks = new OleDbParameter("@Remarks", documentVersion.Remarks);
+            OleDbParameter locationPDF = new OleDbParameter("@LocationPDF", documentVersion.Location_PDF);
+            OleDbParameter locationEditable = new OleDbParameter("@LocationEditable", documentVersion.Location_Editable);
+
+            command.Parameters.Add(documentId);
+            command.Parameters.Add(versionNumber);
+            command.Parameters.Add(effectiveDate);
+            command.Parameters.Add(progress);
+            command.Parameters.Add(descriptionOfChange);
+            command.Parameters.Add(purposeOfChange);
+            command.Parameters.Add(requestor);
+            command.Parameters.Add(remarks);
+            command.Parameters.Add(locationPDF);
+            command.Parameters.Add(locationEditable);
+
+            await command.ExecuteNonQueryAsync();
+            dbConnection.Close();
+
+            return documentVersion;
+        }
+
+        public async void RemoveDocumentVersion(DocumentVersion documentVersion)
+        {
+            string sql = "UPDATE Versions SET IsRemoved = @IsRemoved WHERE ID = " + documentVersion.Id;
+
+            dbConnection.Open();
+            command = new OleDbCommand(sql, dbConnection);
+
+            OleDbParameter isRemoved = new OleDbParameter("@IsRemoved", documentVersion.IsRemoved);
+            command.Parameters.Add(isRemoved);
+
+            await command.ExecuteNonQueryAsync();
+            dbConnection.Close();
+        }
+    }
+}
