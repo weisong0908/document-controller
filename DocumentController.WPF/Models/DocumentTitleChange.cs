748c76a48f5bd21dccd8be1ba0c7a08c321bc20e
Teng Wei Song <hi@tengweisong.com>
2019-03-13 11:52:58 +0800

    Added new model for document title change

---
+++ DocumentController.WPF/Models/DocumentTitleChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentController.WPF.Models
+{
+    public class DocumentTitleChange
+    {
+        public int Id { get; set; }
+        public int DocumentId { get; set; }
+        public int DocumentVersionId { get; set; }
+        public string OriginalDocumentTitle { get; set; }
+        public string NewDocumentTitle { get; set; }
+    }
+}
